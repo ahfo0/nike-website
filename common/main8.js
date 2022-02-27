5c86543bac558a4bff83ed36dbd4a0ad110f11d7
ahfo0 <92067975+ahfo0@users.noreply.github.com>
2022-02-27 19:36:06 +0900

    json data 수정
    json data 수정

--- common/main8.js
+++ common/main8.js
@@ -1,3 +1,5 @@
+window.addEventListener('DOMContentLoaded', function() { 
+ 
  // $.ajax(url[,setting]);
  $.ajax({
     url :  "common/main8_shoes.json",//파일 위치
@@ -5,23 +7,22 @@
     dataType: "json", // 데이터 형식 : xml, json, html, script
     //success: 파일을 잘 불러 왔으면
     success: function(data){// 라이브 서버에서만 확인 가능
-        
-        console.log(data);
         if(data.length>0){
             for (i=0; i<9; i++){
                 let tags = data[i].tags;
                 let name = data[i].name;
                 let type = data[i].type;
                 let price = data[i].price;
-                console.log(tags);
-                console.log(name);
-                console.log(type);
-                console.log(price);
-
-                $('#featured').append(`<div id="${tags}" class="swiper-slide">
-            <div class="shoes" style="cursor: pointer;" onclick="location.href='#';"></div><p>${name}</p><p>${type}</p><p>${price}</p></div>`);
-                
+                $('#featured').append(`
+                <div id="${tags}" class="swiper-slide">
+                    <div class="shoes" style="cursor: pointer;" onclick="location.href='#';"></div>
+                    <p>${name}</p>
+                    <p>${type}</p>
+                    <p>${price}</p>
+                </div>`); 
             }
         }
     }
+});
+
 });
--- common/swiper.js
+++ common/swiper.js
@@ -1,20 +1,6 @@
-/*
-var swiper = new Swiper(".mySwiper", {
-    speed: 600,
-    parallax: true,
-    pagination: {// 하단 버튼
-      el: ".swiper-pagination",
-      clickable: true,// 클릭해서 해당 페이지로 이동
-      type: 'fraction',
-    },
-    navigation: {// 좌우 화살표 버튼
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-*/
-
 const swiper = new Swiper('.swiper', {
+    // 스와이퍼 초기화
+    observer: true, observeParents: true,
     speed: 600,
     parallax: true,
     loop: true, // 마지막에서 최초로 이동
@@ -29,14 +15,12 @@ const swiper = new Swiper('.swiper', {
     breakpoints: {
       1260: {
         slidesPerView: 3,
-        spaceBetween: 30,
+        spaceBetween: 30
       },
       2060: {
         slidesPerView: 5,
         spaceBetween: 10
       },
-    },
-    // 스와이퍼 초기화 (json으로 div까지 통째로 생성했기에
-    // 작동하지 않던 스와이퍼를 초기화 시켜서 적용하게함)
-    observer: true, observeParents: true,
+    }
+    
   })
